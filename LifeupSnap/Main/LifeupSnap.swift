5b5cc0202e10975420b94f307cd50eeb33493f88
Khwan Siricharoenporn <card@lifeup.cool>
2018-11-28 13:53:48 +0700

    [modify][success]Bug fix at LifeupSnap in viewController

--- LifeupSnap/Main/LifeupSnap.swift
+++ LifeupSnap/Main/LifeupSnap.swift
@@ -11,12 +11,12 @@ import UIKit
 public class LifeupSnap: NSObject {
     public static let shared: LifeupSnap = LifeupSnap()
     
-    public weak var delegate: LFSSnapDelegate? = nil
+    public weak var delegate: LFSSnapDelegate?
     
     public lazy var viewController: UIViewController = {
         let bundle = Bundle(for: LFSSnapViewController.self)
         let lfsSnapViewController = LFSSnapViewController(nibName: LFSConstants.LFSNibID.Snap.lfsSnapViewController, bundle: bundle)
-        lfsSnapViewController.delegate = delegate
+        lfsSnapViewController.delegate = self.delegate
         
         return lfsSnapViewController
     }()
