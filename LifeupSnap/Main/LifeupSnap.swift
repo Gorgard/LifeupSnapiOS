e4d63fb5653b38d902866d8ff9f7a3099b91ac5d
Khwan Siricharoenporn <card@lifeup.cool>
2018-11-28 14:17:02 +0700

    [modify][success]Remove delegate in LifeupSnap

--- LifeupSnap/Main/LifeupSnap.swift
+++ LifeupSnap/Main/LifeupSnap.swift
@@ -11,13 +11,10 @@ import UIKit
 public class LifeupSnap: NSObject {
     public static let shared: LifeupSnap = LifeupSnap()
     
-    public weak var delegate: LFSSnapDelegate?
-    
     public lazy var viewController: UIViewController = {
         let bundle = Bundle(for: LFSSnapViewController.self)
         let lfsSnapViewController = LFSSnapViewController(nibName: LFSConstants.LFSNibID.Snap.lfsSnapViewController, bundle: bundle)
-        lfsSnapViewController.delegate = delegate
-        
+
         return lfsSnapViewController
     }()
 }
