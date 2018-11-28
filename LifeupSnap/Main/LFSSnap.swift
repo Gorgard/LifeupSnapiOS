07bf7d31ec5f5f8932655963bd3d4602ac95a103
Khwan Siricharoenporn <card@lifeup.cool>
2018-11-28 14:22:30 +0700

    [modufy][success]Delete lifeupSnap use LFSSnap instead

--- LifeupSnap/Main/LFSSnap.swift
+++ LifeupSnap/Main/LFSSnap.swift
@@ -1,5 +1,5 @@
 //
-//  LifeupSnap.swift
+//  LFSSnap.swift
 //  LifeupSnap
 //
 //  Created by lifeup on 28/11/2561 BE.
@@ -8,13 +8,14 @@
 
 import UIKit
 
-public class LifeupSnap: NSObject {
-    public static let shared: LifeupSnap = LifeupSnap()
+class LFSSnap: NSObject {
+    public static let shared: LFSSnap = LFSSnap()
     
     public lazy var viewController: UIViewController = {
         let bundle = Bundle(for: LFSSnapViewController.self)
         let lfsSnapViewController = LFSSnapViewController(nibName: LFSConstants.LFSNibID.Snap.lfsSnapViewController, bundle: bundle)
-
+        
         return lfsSnapViewController
     }()
 }
+
