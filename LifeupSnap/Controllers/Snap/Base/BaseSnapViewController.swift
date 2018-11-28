875b2d75208ca2fb0cbe154f1cb82746e50bf426
Khwan Siricharoenporn <card@lifeup.cool>
2018-11-28 10:42:19 +0700

    [modify][success]Initial Framework

---
+++ LifeupSnap/Controllers/Snap/Base/BaseSnapViewController.swift
@@ -0,0 +1,33 @@
+//
+//  BaseSnapViewController.swift
+//  LifeupSnap
+//
+//  Created by lifeup on 28/11/2561 BE.
+//  Copyright © 2561 Khwan Siricharoenporn. All rights reserved.
+//
+
+import UIKit
+
+public class BaseSnapViewController: UIViewController {
+    @IBOutlet weak var coverSnapView: UIView!
+    @IBOutlet weak var lineInCoverSnapView: UIView!
+    @IBOutlet weak var snapView: UIView!
+    @IBOutlet weak var snapButton: UIButton!
+    @IBOutlet weak var flipButton: UIButton!
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+
+    override public func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    fileprivate func setupViews() {
+        coverSnapView.layer.cornerRadius = coverSnapView.bounds.size.height / 2
+        lineInCoverSnapView.layer.cornerRadius = lineInCoverSnapView.bounds.size.height / 2
+        snapView.layer.cornerRadius = snapView.bounds.size.height / 2
+    }
+}
